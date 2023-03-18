edbcbb94e9dff504e534f654420b2ed81e341afb
Karlmac Yerima <yerimakarlmac@gmail.com>
2023-03-18 17:05:11 -0500

    edits index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+const currentUser = "Mark";
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// console.log(shortGreeting);
+// console.log(welcomeMessage);
+// console.log(excitedWelcomeMessage);
